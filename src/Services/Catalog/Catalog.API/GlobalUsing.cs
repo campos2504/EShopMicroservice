573799ea6cc045cb3992115d7d52e129434c024d
Pedro Miranda <pcamposmiranda@gmail.com>
2024-07-24 23:34:01 -0300

    feat: Add product creation feature, update project references and add Docker Compose project

---
+++ src/Services/Catalog/Catalog.API/GlobalUsing.cs
@@ -0,0 +1,6 @@
+﻿global using MediatR;
+global using Mapster;
+global using Carter;
+global using Catalog.API.Models;
+global using CoreLibrary.CQRS;
+global using Marten;
---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,30 @@
+﻿using Catalog.API.Models;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, string Description, List<string> Category, decimal Price, string ImageFile)
+        : ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+    internal class CreateProductCommandHandler(IDocumentSession session)
+        : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            var product = new Product
+
+            {
+                Name = command.Name,
+                Description = command.Description,
+                Category = command.Category,
+                Price = command.Price,
+                ImageFile = command.ImageFile
+            };
+
+            session.Store(product);
+            await session.SaveChangesAsync(cancellationToken);
+
+            return new CreateProductResult(product.Id);
+
+        }
+    }
+}
