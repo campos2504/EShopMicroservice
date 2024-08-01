8deecd96fad793a2e05b6615bd243094dc03bc89
Pedro Miranda <pcamposmiranda@gmail.com>
2024-08-01 20:38:40 -0300

    feat: Add StoreBasket feature to Basket API

--- src/Services/Basket/Basket.API/Data/IBasketRepository.cs
+++ src/Services/Basket/Basket.API/Data/IBasketRepository.cs
@@ -3,6 +3,6 @@
     public interface IBasketRepository
     {
         Task<ShoppingCart> GetBasket(string userName, CancellationToken cancellationToken = default);
-
+        Task<ShoppingCart> StoreBasket(ShoppingCart cart, CancellationToken cancellationToken = default);
     }
 }
--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,7 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
 
-
+var assembly = typeof(Program).Assembly;
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+builder.Services.AddValidatorsFromAssembly(assembly);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
-
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 app.Run();
