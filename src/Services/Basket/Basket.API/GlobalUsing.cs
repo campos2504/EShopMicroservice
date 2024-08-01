c8deba38749619f7c81133424b08d6faa84d8713
Pedro Miranda <pcamposmiranda@gmail.com>
2024-07-31 21:03:04 -0300

    feat: Update launchSettings.json and add GetBasket endpoit and handler

---
+++ src/Services/Basket/Basket.API/GlobalUsing.cs
@@ -0,0 +1,13 @@
+﻿global using Basket.API.Models;
+global using CoreLibrary.CQRS;
+global using Carter;
+global using MediatR;
+global using Mapster;
+global using FluentValidation;
+global using CoreLibrary.Behaviors;
+global using CoreLibrary.Exceptions;
+//global using Basket.API.Exceptions;
+global using Marten;
+global using Basket.API.Data;
+global using CoreLibrary.Exceptions.Handler;
+global using Basket.API.Dtos;
