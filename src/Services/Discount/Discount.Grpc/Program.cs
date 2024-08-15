111625ef266fca4d10a36b52bb9e9c5efffc76d5
Pedro Miranda <pcamposmiranda@gmail.com>
2024-08-15 00:10:35 -0300

    chore: Update Discount.Grpc project to use discount.proto

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,4 +1,6 @@
 
+using Discount.Grpc.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddGrpc();
@@ -8,6 +10,7 @@ var app = builder.Build();
 
 //app.MapGrpcService<GreeterService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+app.MapGrpcService<DiscountService>();
 if (app.Environment.IsDevelopment())
 {
     app.MapGrpcReflectionService();
