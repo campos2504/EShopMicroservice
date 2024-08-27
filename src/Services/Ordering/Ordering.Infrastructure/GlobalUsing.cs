43628177da3be12baac7a914c4d8a8b8de6304b7
Pedro Miranda <pcamposmiranda@gmail.com>
2024-08-27 20:44:13 -0300

    chore: Added CustomerConfiguration, OrderConfiguration, OrderItemConfiguration, and ProductConfiguration in Ordering.Infrastructure

---
+++ src/Services/Ordering/Ordering.Infrastructure/GlobalUsing.cs
@@ -0,0 +1,6 @@
+global using Ordering.Domain.Enums;
+global using Microsoft.EntityFrameworkCore;
+global using Ordering.Domain.Models;
+global using Ordering.Domain.ValueObjects;
+global using Ordering.Domain.Abstractions;
+global using Microsoft.EntityFrameworkCore.Metadata.Builders;
